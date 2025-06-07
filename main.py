e97eb247ab6627f51add4054bd9ba95f92f908b3
santiago <santiagodeandrade4@gmail.com>
2025-06-06 22:53:46 -0400

    Avances en entrenamiento y evaluacion

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from keras import callbacks
 from keras.src.backend.config import max_epochs
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from utils.model_builder import model_builder
 import keras_tuner as kt
+from tensorflow.keras.callbacks import EarlyStopping
 
 # carga de datos
 (X_train, Y_train), (X_test, Y_test) =  keras.datasets.fashion_mnist.load_data()
@@ -26,11 +28,15 @@ X_test  = X_test / 255.0
 tuner = kt.Hyperband(
     model_builder,
     objective='val_precision',
-    max_epochs=10,
+    max_epochs=15,
     directory="train_results",
-    project_name="Fashion"
+    project_name="Fashion",
+    factor=2
+
+
 )
 
+print(" ~~~~~~~~ Espacio de busqueda de hiperparametros")
 tuner.search_space_summary()
 
 tuner.search(
@@ -42,9 +48,27 @@ tuner.search(
 
 best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 print("Mejores hiperparametros")
-print(best_hps)
+print(best_hps.values)
 
-model = tuner.hypermodel.build(best_hps)
-# Entrenar el modelo final con los mejores hiperparámetros
-model.fit(X_train, Y_train, epochs=15, validation_data=(X_val, Y_val))
+early_stopping = EarlyStopping(
+    monitor='val_precision',  # Métrica a monitorear
+    mode='max',              # Queremos maximizar la precisión
+    patience=15,             # Número de épocas sin mejora antes de detener
+    verbose=1,
+    restore_best_weights=True  # Crucial: recupera los pesos del mejor modelo
+)
+
+
+best_model = tuner.hypermodel.build(best_hps)
+best_model.fit(
+    X_train, Y_train,
+    validation_data=(X_val, Y_val),
+    epochs=20,
+    callbacks=[early_stopping]
+)
+
+test_loss, test_accuracy, test_precision = best_model.evaluate(X_test, Y_test)
+print(f"Loss en test: {test_loss}")
+print(f"Accuracy en test: {test_accuracy}")
+print(f"Precision en test: {test_precision}")
 
